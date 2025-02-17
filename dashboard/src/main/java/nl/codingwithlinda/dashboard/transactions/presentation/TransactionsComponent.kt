d850e067ff5ffc29a5b0c860e44462aa00212b8c
ldporsius <linda.d.porsius@gmail.com>
2025-02-17 16:59:34 +0100

    transactions 2

--- dashboard/src/main/java/nl/codingwithlinda/dashboard/transactions/presentation/TransactionsComponent.kt
+++ dashboard/src/main/java/nl/codingwithlinda/dashboard/transactions/presentation/TransactionsComponent.kt
@@ -1,14 +1,20 @@
 package nl.codingwithlinda.dashboard.transactions.presentation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import nl.codingwithlinda.core.test_data.fakeTransactions
 import nl.codingwithlinda.core_ui.SpendLessTheme
+import nl.codingwithlinda.core_ui.primaryFixed
 import nl.codingwithlinda.dashboard.transactions.presentation.ui_model.TransactionUi
 import nl.codingwithlinda.dashboard.transactions.presentation.ui_model.mapping.toUi
 
@@ -18,11 +24,24 @@ fun TransactionsComponent(
     transactions: List<TransactionUi>
 ) {
 
+    val context = LocalContext.current
     LazyColumn(
         modifier = modifier
     ) {
         items(transactions){transaction ->
-            Text(transaction.title)
+            Row {
+                Box(modifier = Modifier
+                    .background(
+                        color = primaryFixed,
+                        shape = MaterialTheme.shapes.medium
+                    )
+                ){
+                    Text(transaction.imageText(context))
+                }
+
+                Text(transaction.title)
+            }
+
         }
     }
 }
--- dashboard/src/main/java/nl/codingwithlinda/dashboard/transactions/presentation/ui_model/TransactionUi.kt
+++ dashboard/src/main/java/nl/codingwithlinda/dashboard/transactions/presentation/ui_model/TransactionUi.kt
@@ -1,6 +1,8 @@
 package nl.codingwithlinda.dashboard.transactions.presentation.ui_model
 
+import android.content.Context
 import nl.codingwithlinda.core.domain.model.ExpenseCategory
+import nl.codingwithlinda.dashboard.categories.presentation.mapping.expenseCategoriesToUi
 
 data class TransactionUi(
     val amount: String = "-$59.99",
@@ -8,5 +10,13 @@ data class TransactionUi(
     val title: String = "Adobe Yearly",
     val description: String = "Enjoyed a coffee and a snack at Starbucks with Rick and M.",
     val category: ExpenseCategory = ExpenseCategory.HOME,
+    ){
 
-    )
+    fun imageText(
+        context: Context
+    ): String{
+        return expenseCategoriesToUi(context).find {
+            it.expenseCategory == category
+        }?.imageText ?: ""
+    }
+}
